69f014536ce020f45c166a90b13aa2326f13c1dc
Dominick Martelly <dominick.martelly@gmail.com>
2016-12-04 00:14:43 -0500

    Removed null user form the init-db gulp file

--- Code/scripts/init-db.js
+++ Code/scripts/init-db.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var config = require('../src/config');
 
-var User = require('../src/web/models/user');
-var Agency = require('../src/web/models/agency');
+var User = require('../src/models/user');
+var Agency = require('../src/models/agency');
 
 mongoose.connect(config.db);
 mongoose.Promise = require('bluebird');
@@ -17,54 +17,36 @@ var nullAgency = new Agency({
     phone: '0000000000',
     rank: 'n/a',
 });
+
+var rootUser = new User({
+    username: 'root',
+    firstname: 'N/A',
+    lastname: 'N/A',
+    password: config.rootPasswd,
+    email: 'null@null.com',
+    tier: 'ROOT',
+    badge: '0',
+    unit: 'N/A',
+    rank: 'N/A',
+    agency: agency._id
+});
+
 nullAgency.save(function (err, agency) {
     if (err) {
-        console.log('Null Agency could not save:' + err);
+        console.log('Null Agency could not be saved:' + err);
         process.exit(1);
     } else {
         console.log('Null Agency has been registered: ' + agency);
-        var nullUser = new User({
-            username: 'NULL',
-            firstname: 'n/a',
-            lastname: 'n/a',
-            password: '1234',
-            email: 'null@null.com',
-            tier: 'OFFICER',
-            badge: '0',
-            unit: 'n/a',
-            rank: 'n/a',
-            agency: agency._id
-        });
-        User.createUser(nullUser, function (err, user) {
+        rootUser.agency = agency._id;
+        User.createUser(rootUser, function (err, user) {
             if (err) {
-                console.log('null user could not save' + err);
-                process.exit(1);
+                console.log('root user could not be saved: ' + err);
+                process.exit(2);
             }
             else {
-                console.log('null user has been registered: ' + user);
-                var rootUser = new User({
-                    username: 'root',
-                    firstname: 'n/a',
-                    lastname: 'n/a',
-                    password: config.rootPasswd,
-                    email: 'null@null.com',
-                    tier: 'ROOT',
-                    badge: '0',
-                    unit: 'n/a',
-                    rank: 'n/a',
-                    agency: agency._id
-                });
-                User.createUser(rootUser, function (err, user) {
-                    if (err) {
-                        console.log('root user could not save' + err);
-                        process.exit(1);
-                    }
-                    else {
-                        console.log('root user has been registered: ' + user);
-                        console.log('The database has been initialized');
-                        process.exit(0);
-                    }
-                })
+                console.log('root user has been registered: ' + user);
+                console.log('The database has been initialized');
+                process.exit(0);
             }
         })
     }
