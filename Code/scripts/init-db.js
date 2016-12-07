5888ebc09c71b94897610f5f3059b226ac8781f7
Dominick Martelly <dominick.martelly@gmail.com>
2016-12-07 03:01:56 -0500

    Updated gulp files and readme

--- Code/scripts/init-db.js
+++ Code/scripts/init-db.js
@@ -3,6 +3,7 @@ var config = require('../src/config');
 
 var User = require('../src/models/user');
 var Agency = require('../src/models/agency');
+var Category = require('../src/models/category');
 
 mongoose.connect(config.db);
 mongoose.Promise = require('bluebird');
@@ -31,6 +32,29 @@ var rootUser = new User({
     rank: 'N/A'
 });
 
+//Categories
+
+var autoCategory = new Category({
+    name: 'Auto Theft',
+    fields: ['Year', 'Make', 'Model', 'Style', 'Color',
+        'VIN Number', 'License Plate State', 'License Plate Number']
+});
+
+var boatCategory = new Category({
+    name: 'Boat Theft',
+    fields: ['Year', 'Manufacturer', 'Model', 'Type', 'Length - Feet',
+        'Color', 'Hull Identification Number', 'Registration State', 'Registration Number',
+        'Propulsion Type', 'Propulsion Make', 'Trailer Manufacturer',
+        'VIN Number', 'License Plate State', 'License Plate Number']
+});
+
+var generalCategory = new Category({
+    name: 'General',
+    fields: ['Category (Arson, Missing Person, ...)', 'First Name', 'Last Name',
+        'Date of Birth (DD/MM/YYYY)', 'Drivers License Number', 'Address', 'Zip Code',
+        'Race', 'Sex', 'Height', 'Weight', 'Hair Color', 'Tattoos']
+});
+
 nullAgency.save(function (err, agency) {
     if (err) {
         console.log('Null Agency could not be saved:' + err);
@@ -38,15 +62,36 @@ nullAgency.save(function (err, agency) {
     } else {
         console.log('Null Agency has been registered: ' + agency);
         rootUser.agency = agency._id;
-        rootUser.save(function (err, user) {
+        User.createUser(rootUser, function (err, user) {
             if (err) {
                 console.log('root user could not be saved: ' + err);
                 process.exit(2);
-            }
-            else {
+            } else {
                 console.log('root user has been registered: ' + user);
-                console.log('The database has been initialized');
-                process.exit(0);
+                autoCategory.save(function (err) {
+                    if (err) {
+                        console.log('Error saving categories: ' + err);
+                        process.exit(3);
+                    } else {
+                        boatCategory.save(function (err) {
+                            if (err) {
+                                console.log('Error saving categories: ' + err);
+                                process.exit(3);
+                            } else {
+                                generalCategory.save(function (err) {
+                                    if (err) {
+                                        console.log('Error saving categories: ' + err);
+                                        process.exit(3);
+                                    } else {
+                                        console.log('3 Categories have been registered: ' + user);
+                                        console.log('The database has been initialized');
+                                        process.exit(0);
+                                    }
+                                })
+                            }
+                        })
+                    }
+                });
             }
         })
     }
--- README.md
+++ README.md
@@ -22,56 +22,35 @@ The current application stack consists of:
 This app is designed to be used in a Node.js environment. Version 6 of
 the BOLO Flier Creator is designed to be used with a MongoDB server.
 
-### Running the application
-
-The application is ran from Code/scr/web/app.js
+## Running the application
 
 ### Running MongoDB
 
 A mongoDB server is need form the application to connect to. A server
-can be started locally
+can be started locally for testing and for the final product.
+
+MongoDB can be downloaded from https://www.mongodb.com/. The Application
+was ran and tested under MongoDB v3.2.0, but should be runnable on the
+latest version of MongoDB
+
+### Running Node.js
+
+Node/js can be downloaded from https://nodejs.org/en/. The Application
+was ran and tested under Node v4.4.7
+
+The application is ran from Code/scr/app.js using node.js. The
+
+
 
 ## Development Notes
 
 ### Secrets
 In order for the application to run on a local machine your development
-environment needs to have specific Cloudant credentials set. All you need to
-do is save a file named .env in your project root with the needed environment
-variables set. Check the .env.example file to see the format and variables
-used in this project.  **Do not edit the .env.example file and/or commit it.**
-
-### Testing
-This project is using the following libraries for testing:
-
-* [Mocha Test Runner](https://mochajs.org/)
-* [Chai Assertion Library](http://chaijs.com/)
-* [Sinon.JS Test Double Library](http://sinonjs.org)
-* [Mockery - Node.js require() Mocking Library](https://github.com/mfncooper/mockery)
-
-The project is set up for an effective BDD/TDD workflow. Tests are contained
-in the test/ directory which contain directories for unit, functional, and
-acceptance tests.
-
-To execute Functional and Unit tests with coverage reports:
-`npm test`
-
-Coverage reports are stored in `./coverage` directory.
-
-To run individual tests:
-`mocha test/unit` -or- `npm run unit-test`
-`mocha test/integration`
-`mocha test/functional`
-
-The --watch flag can be used to watch for any changes to tests during
-development:
-`mocha --watch test/unit`
-`mocha --watch test/integration`
-`mocha --watch test/functional`
-
-Note that acceptance tests have not been implemented yet. The project plans
-to use WebDriver for accpetance tests against user story scenarios. Changes
-to these plans will be noted as needed.
-
+environment needs to have specific Sendgrid credentials set. All you
+need to do is save a file named .env in your project root with the
+needed API key set.
+Example: "SENDGRID_API_KEY = SG.uSrswVRAQ9im..."
+*Do not edit the .env.example file and/or commit it.*
 
 ## Documentation
 
@@ -82,5 +61,3 @@ The project requires all source code to be documented using
 Generating the documentation is easy! Make sure that JSDoc is installed by
 using the `npm install` command. Then type `npm run docs` and that's it.
 Open jsdoc/index.html file in your browser to read. Enjoy.
-
-## References
